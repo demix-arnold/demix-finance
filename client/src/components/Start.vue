593a939c3eefb27ecf4744a78419ec05756f5316
Lina-Jin <104341577+Lina-Jin@users.noreply.github.com>
2022-07-19 16:10:19 +0900

    1
    1

--- client/src/components/Start.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section class="start scroll-spy">
-    <div class="inner">
-      <div class="title center">Start earning today</div>
-      <a class="btn centerW" href="mailto:contact@deliquidity.com">문의하기</a>
-    </div>
-  </section>
-</template>
