c1cd60565b9bd1478161758e21623d1e34605f82
tejin3 <tejin3@naver.com>
2022-07-25 12:00:36 +0900

    차트 js 이동

--- client/src/components/Project.vue
+++ client/src/components/Project.vue
@@ -9,8 +9,8 @@
             <div class="strategyTitle smallTitle">
               투자전략1 (Spot, LEVERAGE: ⅹ1)
             </div>
-            <div class="projectName">AUTO/USDT (ⅹ1)</div>
-            <div class="projectInfo">
+            <!-- <div class="projectName">AUTO/USDT (ⅹ1)</div> -->
+            <div class="projectInfo mt10">
               대상: 상대적으로 낮은 유동성을 가진 마켓에 적합<br />
               규모: $100,000 이하 소규모 자산<br />
               특징: 낮은 리스크, 안정적인수익<br />
@@ -20,15 +20,15 @@
           <div class="rela">
             <slot name="chart1"></slot>
             <slot name="chart11"></slot>
-            <div class="p-ab1">Operating Time (H)</div>
+            <div class="p-ab1">APY: {{ apyList[1] }}%</div>
           </div>
         </li>
         <li class="project project2">
           <div class="text">
             <span class="badge highVolatility">High Volatility</span>
             <div class="strategyTitle smallTitle">투자 전략 2 (Spot, x3)</div>
-            <div class="projectName">LUNC/USDT (x3)</div>
-            <div class="projectInfo">
+            <!-- <div class="projectName">LUNC/USDT (x3)</div> -->
+            <div class="projectInfo mt10">
               대상: 높은 변동성을 가진 마켓에 적합
               <br />
               규모: $200,000 내외 중소규모 자산<br />특징: 높은 변동성을 활용한
@@ -40,7 +40,7 @@
           <div class="rela">
             <slot name="chart2"></slot>
             <slot name="chart22"></slot>
-            <div class="p-ab1">Operating Time (H)</div>
+            <div class="p-ab1">APY: {{ apyList[2] }}%</div>
           </div>
         </li>
         <li class="project project3">
@@ -49,8 +49,8 @@
             <div class="strategyTitle smallTitle">
               투자 전략 3 (Future, x5~x10)
             </div>
-            <div class="projectName">LUNA2/BUSD (x10)</div>
-            <div class="projectInfo">
+            <!-- <div class="projectName">LUNA2/BUSD (x10)</div> -->
+            <div class="projectInfo mt10">
               대상: 매우 높은 변동성을 가진 마켓에 적합 하락장(장기 우하향),
               다양한 전략 구사 가능
               <br />
@@ -63,7 +63,7 @@
           <div class="rela">
             <slot name="chart3"></slot>
             <slot name="chart33"></slot>
-            <div class="p-ab1">Operating Time (H)</div>
+            <div class="p-ab1">APY: {{ apyList[3] }}%</div>
           </div>
         </li>
         <li class="project project4">
@@ -72,8 +72,8 @@
             <div class="strategyTitle smallTitle">
               투자 전략 4 (Delivery, x5~x10)
             </div>
-            <div class="projectName">BTC/USD (x10)</div>
-            <div class="projectInfo">
+            <!-- <div class="projectName">BTC/USD (x10)</div> -->
+            <div class="projectInfo mt10">
               대상: 풍부한 유동성을 가지고 장기 우상향 마켓에 적합<br />
               규모: $200,000 이상의 대규모 자산<br />특징: 회복 및 상승장에서
               코인 수량을 점진적으로 증가 높은 리스크의 선물 시장에서 디믹스
@@ -86,7 +86,7 @@
           <div class="rela">
             <slot name="chart4"></slot>
             <slot name="chart44"></slot>
-            <div class="p-ab1">Operating Time (H)</div>
+            <div class="p-ab1">APY: {{ apyList[4] }}%</div>
           </div>
         </li>
         <!-- <li class="project project5">
@@ -115,3 +115,27 @@
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  props: {
+    apyList: Array,
+  },
+  // created() {},
+  // mounted() {
+  //   this.list = this.apyList;
+  //   console.log(this.list);
+  //   console.log(this.apyList);
+  // },
+  // computed: {
+  //   text1() {
+  //     return this.apyList[1];
+  //   },
+  // },
+  // data() {
+  //   return {
+  //     list: [],
+  //   };
+  // },
+};
+</script>
